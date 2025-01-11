e0775b5f28c5af3dbd77a2f694042a6549bfb803
OldBear0625 <med.k0625@gmail.com>
2025-01-11 14:57:03 +0500

    Update figures.hpp

--- task2/figures.hpp
+++ task2/figures.hpp
@@ -1,10 +1,12 @@
-#pragma once
-
 #include <memory>
 #include <stdexcept>
 
 enum class FigureType
 {
+// Make PI accessible to tests and other code
+constexpr double PI = 3.14;
+
+enum class FigureType {
     TRIANGLE,
     CIRCLE,
     RECTANGLE,
@@ -12,6 +14,7 @@ enum class FigureType
 
 class Figure {
 public:
+    virtual ~Figure() = default;  // Add virtual destructor for proper cleanup
     virtual FigureType Type() const = 0;
     virtual double Perimeter() const = 0;
     virtual double Area() const = 0;
@@ -20,22 +23,57 @@ public:
 class Rect : public Figure
 {
 };
+class Rect : public Figure {
+private:
+    double width;
+    double height;
 
 class Triangle : public Figure
 {
+public:
+    Rect(double width, double height);
+    FigureType Type() const override;
+    double Perimeter() const override;
+    double Area() const override;
 };
 
 class Circle : public Figure
 {
+class Triangle : public Figure {
+private:
+    double sideA, sideB, sideC;
+
+public:
+    Triangle(double sideA, double sideB, double sideC);
+    FigureType Type() const override;
+    double Perimeter() const override;
+    double Area() const override;
 };
 
 std::unique_ptr<Figure> make_figure(FigureType type, double a, double b = 0, double c = 0);
+class Circle : public Figure {
+private:
+    double radius;
 
 class WrongTriangle : public std::invalid_argument
 {
+public:
+    Circle(double radius);
+    FigureType Type() const override;
+    double Perimeter() const override;
+    double Area() const override;
 };
 
 class LessThanZeroParam : public std::invalid_argument
 {
+std::unique_ptr<Figure> make_figure(FigureType type, double paramA, double paramB = 0, double paramC = 0);
+
+class WrongTriangle : public std::invalid_argument {
+public:
+    using std::invalid_argument::invalid_argument;
 };
 
+class LessThanZeroParam : public std::invalid_argument {
+public:
+    using std::invalid_argument::invalid_argument;
+};
