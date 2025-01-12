f6973e261153814d8a2b150caa5745a7e1651a9f
OldBear0625 <med.k0625@gmail.com>
2025-01-12 21:41:03 +0500

    Update matrix.cpp

--- task1/matrix.cpp
+++ task1/matrix.cpp
@@ -1,14 +1,11 @@
+#include "figures.hpp"
+#include <cmath>
 #include "matrix.hpp"
-#include <iostream>
 #include <stdexcept>
 
-Matrix::Matrix(int rowCount, int colCount) 
-    : m_rows(rowCount), m_cols(colCount) {
-    AllocateMemory();
-}
-
-Matrix::~Matrix() { 
-    DeallocateMemory();
+Rect::Rect(double w, double h) : width(w), height(h) {
+    if (w < 0 || h < 0) {
+        throw LessThanZeroParam();
 Matrix::Matrix(int r, int c) {
     if (r < 0 || c < 0) {
         throw std::out_of_range("");
@@ -24,12 +21,16 @@ Matrix::Matrix(int r, int c) {
     }
 }
 
-void Matrix::Reset(int rowCount, int colCount) {
-    DeallocateMemory();  // Освобождаем старую память
+FigureType Rect::Type() const {
+    return FigureType::RECTANGLE;
+}
 
-    if (rowCount <= 0 || colCount <= 0) {
-        m_rows = 0;
-        m_cols = 0;
+double Rect::Perimeter() const {
+    return (width + height) * 2;
+}
+
+double Rect::Area() const {
+    return width * height;
 void Matrix::Reset(int r, int c) {
     if (r < 0 || c < 0) {
         throw std::out_of_range("");
@@ -39,18 +40,18 @@ void Matrix::Reset(int r, int c) {
         num_cols = 0;
         data.clear();
     } else {
-        m_rows = rowCount;
-        m_cols = colCount;
-        AllocateMemory();
         num_rows = r;
         num_cols = c;
         data.assign(r, std::vector<int>(c, 0));
     }
 }
 
-int& Matrix::At(int row, int col) {
-    CheckBounds(row, col);
-    return elements[row][col];
+Triangle::Triangle(double side1, double side2, double side3) : a(side1), b(side2), c(side3) {
+    if (side1 < 0 || side2 < 0 || side3 < 0) {
+        throw LessThanZeroParam();
+    }
+    if (side1 + side2 <= side3 || side1 + side3 <= side2 || side2 + side3 <= side1) {
+        throw WrongTriangle();
 int Matrix::At(int r, int c) const {
     if (r < 0 || r >= num_rows || c < 0 || c >= num_cols) {
         throw std::out_of_range("");
@@ -58,9 +59,8 @@ int Matrix::At(int r, int c) const {
     return data[r][c];
 }
 
-const int& Matrix::At(int row, int col) const {
-    CheckBounds(row, col);
-    return elements[row][col];
+FigureType Triangle::Type() const {
+    return FigureType::TRIANGLE;
 int& Matrix::At(int r, int c) {
     if (r < 0 || r >= num_rows || c < 0 || c >= num_cols) {
         throw std::out_of_range("");
@@ -68,23 +68,22 @@ int& Matrix::At(int r, int c) {
     return data[r][c];
 }
 
+double Triangle::Perimeter() const {
+    return a + b + c;
 int Matrix::GetRows() const {
-    return m_rows;
     return num_rows;
 }
 
+double Triangle::Area() const {
+    double half_perimeter = Perimeter() / 2;
+    return std::sqrt(half_perimeter * (half_perimeter - a) * (half_perimeter - b) * (half_perimeter - c));
 int Matrix::GetCols() const {
-    return m_cols;
     return num_cols;
 }
 
-bool Matrix::operator==(const Matrix& otherMatrix) const {
-    if (m_rows != otherMatrix.m_rows || m_cols != otherMatrix.m_cols) return false;
-    for (int i = 0; i < m_rows; ++i) {
-        for (int j = 0; j < m_cols; ++j) {
-            if (elements[i][j] != otherMatrix.elements[i][j]) {
-                return false;
-            }
+Circle::Circle(double r) : radius(r) {
+    if (r < 0) {
+        throw LessThanZeroParam();
 std::istream& operator>>(std::istream& in, Matrix& m) {
     int r, c;
     in >> r >> c;
@@ -94,23 +93,11 @@ std::istream& operator>>(std::istream& in, Matrix& m) {
             in >> m.At(i, j);
         }
     }
-    return true;
-}
-
-bool Matrix::operator!=(const Matrix& otherMatrix) const {
-    return !(*this == otherMatrix);
     return in;
 }
 
-Matrix Matrix::operator+(const Matrix& otherMatrix) const {
-    if (m_rows != otherMatrix.m_rows || m_cols != otherMatrix.m_cols) {
-        throw std::invalid_argument("Matrices dimensions do not match");
-    }
-
-    Matrix sumMatrix(m_rows, m_cols);
-    for (int i = 0; i < m_rows; ++i) {
-        for (int j = 0; j < m_cols; ++j) {
-            sumMatrix.At(i, j) = this->At(i, j) + otherMatrix.At(i, j);
+FigureType Circle::Type() const {
+    return FigureType::CIRCLE;
 std::ostream& operator<<(std::ostream& out, const Matrix& m) {
     out << m.GetRows() << " " << m.GetCols() << "\n";
     for (int i = 0; i < m.GetRows(); ++i) {
@@ -119,42 +106,38 @@ std::ostream& operator<<(std::ostream& out, const Matrix& m) {
         }
         out << "\n";
     }
-    return sumMatrix;
     return out;
 }
 
-void Matrix::AllocateMemory() {
-    if (m_rows > 0 && m_cols > 0) {
-        elements = new int*[m_rows]; // Выделяем память для массива
-        for (int i = 0; i < m_rows; ++i) {
-            elements[i] = new int[m_cols]{0};  // Инициализация нулями
-        }
-    } else {
-        elements = nullptr; // Указываем на nullptr, если размеры некорректны
-    }
+double Circle::Perimeter() const {
+    return 2 * PI * radius;
 bool operator==(const Matrix& m1, const Matrix& m2) {
     return m1.num_rows == m2.num_rows && m1.num_cols == m2.num_cols && m1.data == m2.data;
 }
 
-void Matrix::DeallocateMemory() {
-    if (elements) {
-        for (int i = 0; i < m_rows; ++i) {
-            delete[] elements[i]; // Освобождаем каждый из массивов
-        }
-        delete[] elements; // Освобождаем массив
-        elements = nullptr; // Устанавливаем указатель в nullptr для безопасности
-    }
+double Circle::Area() const {
+    return PI * radius * radius;
 bool operator!=(const Matrix& m1, const Matrix& m2) {
     return !(m1 == m2);
 }
 
-void Matrix::CheckBounds(int row, int col) const {
-    if (row < 0 || row >= m_rows || col < 0 || col >= m_cols) {
-        throw std::out_of_range("Index out of range");
+std::unique_ptr<Figure> make_figure(FigureType type, double x, double y, double z) {
+    if (x < 0 || y < 0 || z < 0) {
+        throw LessThanZeroParam();
 Matrix operator+(const Matrix& m1, const Matrix& m2) {
     if (m1.GetRows() != m2.GetRows() || m1.GetCols() != m2.GetCols()) {
         throw std::invalid_argument("");
     }
+
+    switch (type) {
+        case FigureType::RECTANGLE:
+            return std::make_unique<Rect>(x, y);
+        case FigureType::CIRCLE:
+            return std::make_unique<Circle>(x);
+        case FigureType::TRIANGLE:
+            return std::make_unique<Triangle>(x, y, z);
+        default:
+            return nullptr;
     Matrix res(m1.GetRows(), m1.GetCols());
     for (int i = 0; i < m1.GetRows(); ++i) {
         for (int j = 0; j < m1.GetCols(); ++j) {
